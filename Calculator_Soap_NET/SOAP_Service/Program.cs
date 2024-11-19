1cf47394a2c884047a9817d073f967efa269e145
mspadron28 <matiaspadron2@gmail.com>
2024-11-19 10:04:48 -0500

    Se añade el consumo del servicio usando .NET realizado por Matias Padrón y William Leon

---
+++ Calculator_Soap_NET/SOAP_Service/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Threading.Tasks;
+using ServiceCalculator;
+
+namespace SoapClientExample
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            // Se crea una instancia del cliente SOAP
+            var client = new CalculatorSoapClient(CalculatorSoapClient.EndpointConfiguration.CalculatorSoap);
+
+            try
+            {
+                // Realizar operaciones asíncronas con el servicio web
+                int resultadoSuma = await client.AddAsync(8, 8);
+                Console.WriteLine("Resultado de la suma: " + resultadoSuma);
+
+                int resultadoResta = await client.SubtractAsync(48, 8);
+                Console.WriteLine("Resultado de la resta: " + resultadoResta);
+
+                int resultadoMultiplicacion = await client.MultiplyAsync(8, 8);
+                Console.WriteLine("Resultado de la multiplicación: " + resultadoMultiplicacion);
+
+                int resultadoDivision = await client.DivideAsync(8, 2);
+                Console.WriteLine("Resultado de la división: " + resultadoDivision);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Error: " + ex.Message);
+            }
+            finally
+            {
+                // Cerrar el cliente para liberar los recursos
+                await client.CloseAsync();
+            }
+        }
+    }
+}
